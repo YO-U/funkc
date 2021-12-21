b4e896c88d74c67fee639737882118ce272e38d3
enoxy <enoxy@DESKTOP-8BHQKRR>
2021-12-21 09:44:06 +0200

    Добавьте файлы проекта.

---
+++ funkc/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace funkc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+
+           Program.FidRec(k);
+            
+        }
+        static int FidRec(int n )
+        {
+          int F1;
+          int F2;
+          int Fk = 0;
+            for (int i = 2; i < 7; i++)
+            {
+
+                F1 = n - 2;
+                F2 = n - 1;
+                if (F1 == 2 || F2 == 1)
+                {
+                    F1 = 1;
+                    F1 = 1;
+
+                    Fk += (F1) + (F2);
+                    Console.WriteLine(Fk);
+                }
+                else
+                {
+
+                    Fk += (F1) + (F2);
+                    Console.WriteLine(Fk);
+                }
+            }
+            return Fk;
+        }
+    }
+}
